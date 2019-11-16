d13f3726e551ce0179d3a5c56f6cd003970e1e98
Jacob Hayes <github@jakehayes.33mail.com>
2019-11-16 17:55:57 -0500

    Processed API calls into human readable text
    Also added a json file for future cityID support

--- Python_Weather_App 2.0/Python_Weather_App 2.0/Python_Weather_App_2.0.py
+++ Python_Weather_App 2.0/Python_Weather_App 2.0/Python_Weather_App_2.0.py
@@ -23,12 +23,14 @@ Use try blocks when establishing connections to the webservice.  You must print 
 API KEY: 4861192b2ae379f9b65947707449278c
 """
 
+#version 2.2
+
 #------------------------------------------------------------------------#
 ################################DEPENDENCIES##############################
 #------------------------------------------------------------------------#
 
-#city.list.json
-#This solution will contain a city.list.json file that it relies on to convert cities to City IDs
+                    #city.list.json (not yet implemented)
+                    #This solution will contain a city.list.json file that it relies on to convert cities to City IDs
 
 #------------------------------------------------------------------------#
 ##################################IMPORTS#################################
@@ -46,6 +48,12 @@ import uszipcode #to convert zip codes to latitude and longitude
 #------------------------------------------------------------------------#
 #################################FUNCTIONS################################
 #------------------------------------------------------------------------#
+def K_to_C(k):
+    return (k - 273.15)
+
+def C_to_F(c):
+    return (c * 9 / 5 + 32)
+
 
 def Get_Weather(zip_or_city):
     #This function will use the OpenWeatherMap API to send a City ID and retrieve a weather forecast.
@@ -54,7 +62,9 @@ def Get_Weather(zip_or_city):
     #This function first checks to see if it was given a zip code or a city name, then attempts to correlate that with an existing City ID
     #If it cannot, it will alert the user about a failure to connect.
 
-    #try:
+    const_APP_ID = "4861192b2ae379f9b65947707449278c"
+
+    try:
         #Check to see if the string input is able to be converted to a zip code (that is, it is five digits):
         #########is_zip_code = re.match([0-9][0-9][0-9][0-9][0-9], zip_or_city)
         is_zip_code = zip_or_city.isdigit()
@@ -69,50 +79,90 @@ def Get_Weather(zip_or_city):
             parameters = {
                 "lat": lat,
                 "lon": lon,
-                "APPID": "4861192b2ae379f9b65947707449278c"
+                "APPID": const_APP_ID
                 }
+            Print_Weather(parameters)
+
+        else:
+        #if not a zip code, check to see if it matches a city
+            #with open('city.list.json') as city_json:
+                #data = json.load(city_json)
+            
+        #I couldn't get the city list json file to work with python, as it doesn't match the syntax of json files I see when attempting to look up
+        #solutions. For the time being, I will just send the city name in the API call, and attempt to add cityID support later
+            parameters = {
+                "q": zip_or_city,
+                "APPID": const_APP_ID
+                }
+
+            Print_Weather(parameters)
+    except:
+        print("There was an issue retrieving your weather information. Please try again later.")
+
+    
+def Print_Weather(parameters):
+    try:
             #get weather
             weather_response = requests.get("http://api.openweathermap.org/data/2.5/weather", params=parameters)
-            print(weather_response.status_code)
-            print(weather_response.json())
+            if (weather_response.status_code == 404):
+                raise NotFoundError
+            data = weather_response.json()
+
+            #-----#
+            city = data["name"]
+            weather = data["weather"][0]["main"]
+            temp_c = K_to_C(data["main"]["temp"])
+            temp_f = C_to_F(temp_c)
+            #-----#
+            print("Weather right now in " + city + ": " + weather + " with a temperature of " + str(round(temp_c)) + " degrees C (" + str(round(temp_f)) 
+                  + " degrees F).")
 
             #get forecast
             #api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&APPID=4861192b2ae379f9b65947707449278c
             weather_response = requests.get("http://api.openweathermap.org/data/2.5/forecast", params=parameters)
-            print(weather_response.status_code)
-            print(weather_response.json())
+            data = weather_response.json()
+            #print(json.dumps(data, indent=4))
+            Forecast(data, 5)
+            Forecast(data, 13)
+            Forecast(data, 21)
+            Forecast(data, 29)
 
 
-            pass
-        else:
-        #if not a zip code, check to see if it matches a city
-            pass
-        #if so, convert it to city ID using json
+    except:
+        print("Your weather data was unable to be found. Please double-check your input and try again.")
 
-        #if not, return an error to the user
+def Forecast(data, hour):
+    dt = data["list"][hour]["dt_txt"][:10]
+    dweather = data["list"][hour]["weather"][0]["main"]
+    dtemp = data["list"][hour]["main"]["temp"]
 
-    #except:
-        #print("Your weather data was unable to be found. Please double-check your input and try again.")
-    
+    print("Weather on " + dt + " will be " + dweather + " with a temperature of " + str(round(K_to_C(dtemp))) + 
+                  " degrees C at noon (" + str(round(C_to_F(K_to_C(dtemp)))) + " degrees F)")
 
 #------------------------------------------------------------------------#
 ###################################MAIN###################################
 #------------------------------------------------------------------------#
+def main():
+    while 1: #infinite loop allows the user to run the program multiple times
+        print("""Welcome to the
+      ____        _   _                  __        __         _   _                    _                
+     |  _ \ _   _| |_| |__   ___  _ __   \ \      / /__  __ _| |_| |__   ___ _ __     / \   _ __  _ __  
+     | |_) | | | | __| '_ \ / _ \| '_ \   \ \ /\ / / _ \/ _` | __| '_ \ / _ \ '__|   / _ \ | '_ \| '_ \ 
+     |  __/| |_| | |_| | | | (_) | | | |   \ V  V /  __/ (_| | |_| | | |  __/ |     / ___ \| |_) | |_) |
+     |_|    \__, |\__|_| |_|\___/|_| |_|    \_/\_/ \___|\__,_|\__|_| |_|\___|_|    /_/   \_\ .__/| .__/ 
+            |___/                                                                          |_|   |_|    """)
+        zip_or_city = input("Please enter your 5 digit zip code or city name for a weather forecast: ")
+        Get_Weather(zip_or_city)
 
-while 1: #infinite loop allows the user to run the program multiple times
-    print("""Welcome to the
-  ____        _   _                  __        __         _   _                    _                
- |  _ \ _   _| |_| |__   ___  _ __   \ \      / /__  __ _| |_| |__   ___ _ __     / \   _ __  _ __  
- | |_) | | | | __| '_ \ / _ \| '_ \   \ \ /\ / / _ \/ _` | __| '_ \ / _ \ '__|   / _ \ | '_ \| '_ \ 
- |  __/| |_| | |_| | | | (_) | | | |   \ V  V /  __/ (_| | |_| | | |  __/ |     / ___ \| |_) | |_) |
- |_|    \__, |\__|_| |_|\___/|_| |_|    \_/\_/ \___|\__,_|\__|_| |_|\___|_|    /_/   \_\ .__/| .__/ 
-        |___/                                                                          |_|   |_|    """)
-    zip_or_city = input("Please enter your 5 digit zip code or city name for a weather forecast: ")
-    Get_Weather(zip_or_city)
+        #ask the user if they'd like to continue the program
+        again = input("Enter Y to get another forecast, or anything else to quit ")
+        if (again == "Y" or again == "y"): #if they entered anything other than "y" or "Y"
+            pass
+        else:
+            break
 
-    #ask the user if they'd like to continue the program
-    again = input("Enter Y to get another forecast, or anything else to quit ")
-    if (again.upper != "Y"): #if they entered anything other than "y" or "Y"
-        #quit the program
-        break
+
+
+if (__name__ == "__main__"):
+    main()
 
